1a92a9de596b67a4c55ba10a56baba48e4bf3ddb
legend <499458212@qq.com>
2016-10-27 13:59:13 +0800

    Initial Commit

---
+++ shareShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shareShow
+//
+//  Created by 张传奇 on 16/10/27.
+//  Copyright © 2016年 张传奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shareShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shareShow
+//
+//  Created by 张传奇 on 16/10/27.
+//  Copyright © 2016年 张传奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
